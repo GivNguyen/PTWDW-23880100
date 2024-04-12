083a3bc4d8cf429ed58efdc9f579b84c33d9db67
vihandsomeMuBigfan <nggvi0102@gmail.com>
2024-04-12 22:49:35 +0700

    login error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,28 @@
 'use strict'
 
+const passport = require('passport')
 const controller = {}
 
 controller.show = (req, res) => {
-    res.render('login');
+    res.render('login', { loginMessage: req.flash('loginMessage') });
 }
 
-controller.login = (req, res) => {
-
+controller.login = (req, res, next) => {
+    let keepSignedIn = req.body.keepSignedIn;
+    passport.authenticate('local-login', (error, user) => {
+        if (error) {
+            return next(error);
+        }
+        if (!user) {
+            return res.redirect('/users/login')
+        }
+        req.logIn(user, (error) => {
+            if (error) { return next(error); }
+            //luu dang nhap
+            req.session.cookie.maxAge = keepSignedIn ? (24*60*60*1000) : null
+            return res.redirect('/users/my-account')
+        }) 
+    })(req, res, next);
 }
 
 module.exports = controller
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -26,4 +26,7 @@ router.post('/placeorders',
     },
     controller.placeorders
 )
+router.get('/my-account', (req, res) => {
+    res.render('my-account');
+})
 module.exports = router
