f7bce38f239814304b6e08db456f2664872b0647
GivNguyen <nggvi0102@gmail.com>
2024-04-14 23:59:13 +0700

    login validate

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,8 +3,21 @@
 const express = require('express');
 const controller = require('../controllers/authController');
 const router = express.Router()
+const { body, getErrorMessage } = require('../controllers/validator')
+
 
 router.get('/login', controller.show);
-router.post('/login', controller.login)
+router.post('/login', 
+    body('email').trim().notEmpty().withMessage('Email is required').isEmail().withMessage
+    ('Invalid email address'),
+    body('password').trim().notEmpty().withMessage('Email is required'),
+    (req, res, next) => {
+        let message = getErrorMessage(req);
+        if (message) {
+            return res.render('login', { loginMessage: message })
+        }
+    },
+    controller.login
+)
 
 module.exports = router;
